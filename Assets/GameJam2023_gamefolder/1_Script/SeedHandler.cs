2f29b11825f1440fac31feef8006e15d379d7172
chkpcc1a035 <91876308+chkpcc1a035@users.noreply.github.com>
2023-01-31 23:00:07 +0800

    Revert "Create Seed Inital Function and hooked in Cube in Scene1"
    This reverts commit 4f6dd7d2a09aa48a1bbf4c5bafc8d42e739270c0.

--- Assets/GameJam2023_gamefolder/1_Script/SeedHandler.cs
+++ Assets/GameJam2023_gamefolder/1_Script/SeedHandler.cs
@@ -4,24 +4,9 @@ using UnityEngine;
 
 public class SeedHandler : MonoBehaviour
 {
-    int[] initalSeed = { 0, 0, 0, 0 };
-
     // Start is called before the first frame update
     void Start()
     {
-        print("[Seed Value]" + initalSeed[0] + initalSeed[1] + initalSeed[2] + initalSeed[3]);
-
-        void generateSeedValue(int[] seedArray)
-        {
-            for (int loop = 0; loop < seedArray.Length; loop++)
-            {
-                seedArray[loop] = Random.Range(0, 10);
-            }
-
-        }
-        generateSeedValue(initalSeed);
-
-        print("[Seed Value]" + initalSeed[0] + initalSeed[1] + initalSeed[2] + initalSeed[3]);
         
     }
 
@@ -31,4 +16,3 @@ public class SeedHandler : MonoBehaviour
         
     }
 }
-
