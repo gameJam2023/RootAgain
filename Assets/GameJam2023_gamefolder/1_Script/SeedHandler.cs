2117d4320d6fc00622bd17650f6829256cb3e483
chkpcc1a035 <91876308+chkpcc1a035@users.noreply.github.com>
2023-02-01 01:27:41 +0800

    Fix my seed
    Fix my seed

--- Assets/GameJam2023_gamefolder/1_Script/SeedHandler.cs
+++ Assets/GameJam2023_gamefolder/1_Script/SeedHandler.cs
@@ -2,18 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Seed
+{
+    public bool _seedStatus;
+    public int _nutrient;
+    public _seedType _seedType;
+}
+public enum _seedType
+{
+    TypeA,
+    TypeB,
+    TypeC
+};
+
 public class SeedHandler : MonoBehaviour
 {
-
-
+    public List<Seed> seedList = new List<Seed>();
+    // Start is called before the first frame update
     void Start()
     {
 
+        foreach (Seed seed in seedList)
+        {
+            int randomSeedType = Random.Range(0, 10);
+            if (randomSeedType < 3)
+                seed._seedType = _seedType.TypeA;
+            else if (randomSeedType > 3 && randomSeedType < 7)
+                seed._seedType = _seedType.TypeB;
+            else
+                seed._seedType = _seedType.TypeC;
+        }
     }
+
+}
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
